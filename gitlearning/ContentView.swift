5c0ca369d97b525f41784d9c092273e62b6406d3
apple <apple@MacBookAir.lan>
2025-07-28 18:03:27 +0530

    text added as v

--- gitlearning/ContentView.swift
+++ gitlearning/ContentView.swift
@@ -16,7 +16,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, zoople how are you")
-            Text("my name is subin and this is  my branch")
+            Text("my name is subin and this is  my branch v")
                 .font(.title)
                 .foregroundColor(.blue)
 
