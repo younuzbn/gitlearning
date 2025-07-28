dcde04e5398bf81a3e279b974d4bb3dc04f4de32
younuzbn <younuzbn>
2025-07-28 16:30:41 +0530

    initial commit

---
+++ gitlearning/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gitlearning
+//
+//  Created by younuzbn on 28/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gitlearning/gitlearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitlearningApp.swift
+//  gitlearning
+//
+//  Created by younuzbn on 28/07/25.
+//
+
+import SwiftUI
+
+@main
+struct gitlearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
